862c5c9a938fecbbabb13a26a169d65072607732
Kian Shepherd <kianshepherd29@gmail.com>
2016-03-02 09:10:31 -0500

    fixed the draw method

--- Arcade.py
+++ Arcade.py
@@ -49,8 +49,8 @@ class arcade:
         return pygame.mixer.Sound(os.getcwd() + '\\resources\\' + os.path.basename(name).split('.')[0] + '\\' + file) #Fix later; should fetch from __name__ folder
     
     def isColliding(self, obj1, obj2): # checks if two surfaces are collliding
-        rect1 = pygame.Rect(obj1[1], obj1[2], pygame.Surface.get_rect(obj1[0])[2], pygame.Surface.get_rect(obj1[0])[3])
-        rect2 = pygame.Rect(obj2[1], obj2[2], pygame.Surface.get_rect(obj2[0])[2], pygame.Surface.get_rect(obj2[0])[3])
+        rect1 = pygame.Rect(obj1[1], obj1[2], pygame.Surface.get_bounding_rect(obj1[0])[2], pygame.Surface.get_rect(obj1[0])[3])
+        rect2 = pygame.Rect(obj2[1], obj2[2], pygame.Surface.get_bounding_rect(obj2[0])[2], pygame.Surface.get_rect(obj2[0])[3])
         return rect1.colliderect(rect2)
 
     def drawBackground(self, background): # draws background but doesnt update
@@ -61,17 +61,18 @@ class arcade:
         pygame.display.update()
 
     def draw(self, *args): #arg is (surface, x, y)
-        #global previous_areas
-        #update_areas = []
+        global previous_areas
+        update_areas = []
         for arg in args:
             self.screen.blit(arg[0],(arg[1],arg[2]))
-            #area = pygame.Surface.get_bounding_rect(arg[0])
-            #area[0], area[1] = arg[1], arg[2]
-            #update_areas.append(area)
-        #act_rects = update_areas + previous_areas
-        #pygame.display.update(act_rects)
-        #previous_areas = update_areas[:]
-        pygame.display.update()
+            area = pygame.Surface.get_bounding_rect(arg[0])
+            area[0], area[1] = arg[1] - 10, arg[2] - 10
+            area[2], area[3] = area[2] + 20, area[3] + 20
+            update_areas.append(area)
+        act_rects = update_areas[:] + previous_areas[:]
+        pygame.display.update(act_rects)
+        previous_areas = update_areas[:]
+        #pygame.display.update()
 
     def makeSurface(self, width, height, alpha = 0): # creates a surface with transparency 
         if alpha:
