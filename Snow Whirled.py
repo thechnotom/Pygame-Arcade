9d8329c5c05749b1f76589dce5f69799924896f1
Ajfoster4444 <S199725004@OCDSB>
2016-03-03 09:21:18 -0500

    San Jose

--- Snow Whirled.py
+++ Snow Whirled.py
@@ -16,48 +16,66 @@ def SnowWhirled(arcade):
     player = 1
     
     while True:
+        #Declaration of Variables        
         position = 1
         start = 0
         TimeRem = 5000
+        
         while TimeRem > 0:
             
             arcade.getEvents()
+
+            #Timer
             if Start == 1:
                 TimeRem -= 1
+
+            #Key recognition    
             pressed = arcade.getKey()
+            
             if pressed[K_ESCAPE]:
                 pygame.quit()
                 sys.exit()
+                
             if pressed[K_SPACE]:
-                Ttart = 1
-            if position == 1 and pressed[K_DOWN] and not any(pressed[K_LEFT],pressed[K_UP],pressed[K_RIGHT]) and Start == 1:
+                start = 1
+                
+            if position == 1 and pressed[K_DOWN] and not any(pressed[K_LEFT],pressed[K_UP],pressed[K_RIGHT]):
                 position = 2
+
+                #Scoring for players 
                 if  player == 1:
-                    score1 += 90
+                    score1 += 90 
                 elif player == 2:
                     score2 += 90
-            if position == 2 and pressed[K_LEFT] and not any(pressed[K_DOWN],pressed[K_UP],pressed[K_RIGHT]) and Start == 1:
+                    
+            if position == 2 and pressed[K_LEFT] and not any(pressed[K_DOWN],pressed[K_UP],pressed[K_RIGHT]):
                 position = 3
+
+                #Scoring for players 
                 if  player == 1:
                     score1 += 90
                 elif player == 2:
                     score2 += 90
-            if position == 3 and pressed[K_UP] and not any(pressed[K_LEFT],pressed[K_DOWN],pressed[K_RIGHT]) and Start == 1:
+                    
+            if position == 3 and pressed[K_UP] and not any(pressed[K_LEFT],pressed[K_DOWN],pressed[K_RIGHT]):
                 position = 4
+
+                #Scoring for players 
                 if  player == 1:
                     score1 += 90
                 elif player == 2:
                     score2 += 90
-            if position == 4 and pressed[K_RIGHT] and not any(pressed[K_LEFT],pressed[K_UP],pressed[K_DOWN]) and Start == 1:
+                    
+            if position == 4 and pressed[K_RIGHT] and not any(pressed[K_LEFT],pressed[K_UP],pressed[K_DOWN]):
                 position = 1
+
+                #Scoring for players 
                 if  player == 1:
                     score1 += 90
                 elif player == 2:
                     score2 += 90
 
-        if player == 2:
-            break
-        player = 2
+            player = 2
         
 if __name__ == '__main__':
     pygame.init()
