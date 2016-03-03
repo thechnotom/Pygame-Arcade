591bfbd5e4ce1f3f66113454b5316fa20c8ee9bc
AlexCity <s199715013@ocdsb>
2016-03-03 08:29:06 -0500

    Game file added

---
+++ Snow Whirled.py
@@ -0,0 +1,18 @@
+#Snow Whirled
+#Class: ICS4U-02
+#Name: Alex Taylor & Andrew Foster
+#Date: March 3, 2016
+#Description: Snow Whirled
+
+import pygame, os
+from pygame.locals import *
+from Arcade import arcade
+from colours import *
+
+def SnowWhirled():
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.mixer.init(22050,-16,2,1024)
+    pygame.display.set_caption(os.path.basename(__file__).split('.')[0])
+    SnowWhirled
